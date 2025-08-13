b018c3c72ee696a9e46c2705bd875a262ff79884
AryamanTandon18 <tandon.aryaman1@gmail.com>
2025-08-13 18:58:31 +0530

    fixed

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -10,6 +10,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2024-06-20",
 });
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(req: Request) {
   const body = await req.text();
   const signature = headers().get("Stripe-Signature") as string;
